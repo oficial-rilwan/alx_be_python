aa33e30d2949d9b8a783a48210d4b7b11c838720
Rilwan Aribidesi <aribidesirilwan1477@gmail.com>
2024-06-19 15:21:59 +0100

    added all files

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,6 @@
+age = input("How old are you? ")
+difference = 2050 - 2023
+age = int(age) + difference
+
+
+print(f"In 2050, you will be {age} years old.")
---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,4 @@
+hours = 2
+seconds = 1 * 60 * 60 * hours
+
+print(f"2 hour(s) is {seconds} seconds.")
