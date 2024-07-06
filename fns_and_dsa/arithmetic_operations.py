54bcc8dd62a07e6bf5ffe6ba91ab31a609d1758f
Rilwan Aribidesi <aribidesirilwan1477@gmail.com>
2024-07-06 09:52:52 +0100

    added elif statement for zero divisions

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,7 +7,9 @@ def perform_operation(num1, num2, operation):
         case "multiply":
             return num1 * num2
         case "divide":
-            if num2 == 0:
+            if num1 == 0:
+                return 'Cannot devide by zero'
+            elif num2 == 0:
                 return 'Cannot devide by zero'
             else:
                 return num1 / num2
