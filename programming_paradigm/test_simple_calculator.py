917ed070a9dfc3789b012bbecca4f6c3bd68b5a9
Rilwan Aribidesi <aribidesirilwan1477@gmail.com>
2024-07-14 15:41:37 +0100

    update simple test script

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -10,6 +10,12 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
 
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(3, 2), 1)
+        self.assertEqual(self.calc.subtract(-1, 1), -2)
+        self.assertEqual(self.calc.subtract(-1, -1), 0)
+
+
     def test_multiply(self):
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(-1, 1), -1)
@@ -20,7 +26,3 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.divide(-1, 1), -1)
         self.assertEqual(self.calc.divide(-1, -1), 1)
         self.assertIsNone(self.calc.divide(1, 0))
-
-    def test_divide_with_floats(self):
-        self.assertAlmostEqual(self.calc.divide(5, 2), 2.5)
-        self.assertAlmostEqual(self.calc.divide(1, 3), 1 / 3)
