716d1e63e2292b5c9e18b4c646b22f4846ef085b
Rilwan Aribidesi <aribidesirilwan1477@gmail.com>
2024-07-14 15:03:58 +0100

    update account balance

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,9 +1,12 @@
+import sys
+
 class BankAccount:
-    def __init__(self, account_balance):
+    def __init__(self, account_balance = 0):
         self.account_balance = account_balance
 
     def deposit(self, amount):
         self.account_balance = amount + self.account_balance
+
     def withdraw(self, amount):
         if amount > self.account_balance:
             return False
@@ -12,4 +15,4 @@ class BankAccount:
             return True
 
     def display_balance(self):
-       return '$'+self.account_balance 
+       return self.account_balance 
