7eee8adc127c1ad753f8ce74d8c9fa43d77cd037
Rilwan Aribidesi <aribidesirilwan1477@gmail.com>
2024-07-14 14:50:39 +0100

    added bank account file

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,15 @@
+class Bank:
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        self.account_balance = amount + self.account_balance
+    def withdraw(self, amount):
+        if amount > self.account_balance:
+            return False
+        else:
+            self.account_balance = self.account_balance - amount
+            return True
+
+    def display_balance(self):
+       return '$'+self.account_balance 
